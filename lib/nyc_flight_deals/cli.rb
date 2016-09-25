e96bc2bdf4eb234e6f6ef81417d2a919bc123036
christinechoi <smi77y@gmail.com>
2016-09-24 21:55:53 -0400

    add #goodbye to cli, case statement to #menu

--- lib/nyc_flight_deals/cli.rb
+++ lib/nyc_flight_deals/cli.rb
@@ -1,12 +1,13 @@
 class NYCFlightDeals::CLI
 
   def call
-    puts "Today's Flight Deals:"
     list_deals
     menu
+    goodbye
   end
 
   def list_deals
+    puts "Today's Flight Deals:"
     puts <<-DOC
       1. American - $211: Phoenix - New York (and vice versa). RT including all Taxes
       2. American - $165: New York - Denver (and vice versa). RT including all taxes
@@ -14,7 +15,22 @@ class NYCFlightDeals::CLI
   end
 
   def menu
-    puts "Enter the number of the flight you want more info on:"
+    puts "Enter the number of the flight you want more info on, or type 'exit' to exit."
+    while input != "exit"
+
+    input = gets.strip.downcase
+
+      case input 
+      when "1"
+        puts "More info on flight 1..."
+      when "2"
+        puts "More info on flight 2..."
+      end
+    end
+  end
+
+  def goodbye
+    puts "Check back later for more deals. Bye!"
   end
 
 end
